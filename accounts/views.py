ef99fb1c7bd283fb2971ad40777ff4b2eb346385
depproosy <depproosy@gmail.com>
2019-06-13 20:16:28 +0200

    User Registration completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Methods.
 
 def register(request):
   if request.method == "POST":
-   #Register User
-   return
+    #Get from values
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    #Check Validation , If passwords Match
+    if password == password2 :
+       #Check Username
+        if User.objects.filter(username=username).exists():
+          messages.error(request, 'That Username is taken')
+          return redirect('register')
+        else:
+          if User.objects.filter(email=email).exists():
+            messages.error(request, 'That email is already taken')
+            return redirect('register')
+          else:
+            #Looks good
+            user= User.objects.create_user(username=username, email=email, password = password, first_name= first_name, last_name= last_name)  
+            user.save()
+            messages.success('You are nor Registered')
+            return redirect('login')
+    else:  
+     messages.error(request,'passwords do not match')
+     return redirect('register')
   else:
-    return render(request, 'accounts/register.html')
+     return render(request, 'accounts/register.html')
 
 def dashboard(request):
   
